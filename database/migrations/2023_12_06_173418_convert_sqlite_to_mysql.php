17714b8afab04a1b01b889943fdc8108f0380a18
iTzHeXzZ <138040888+iTzHeXzZ@users.noreply.github.com>
2023-12-07 22:14:20 +0100

    final2.0

--- database/migrations/2023_12_06_173418_convert_sqlite_to_mysql.php
+++ database/migrations/2023_12_06_173418_convert_sqlite_to_mysql.php
@@ -190,16 +190,6 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
-            $table->timestamps();
-        });
 
         Schema::create('project_user', function (Blueprint $table) {
             $table->unsignedBigInteger('project_id');
