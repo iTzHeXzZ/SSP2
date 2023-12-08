cee780bf5381caae7c1a052ffd5c03c23001ce84
iTzHeXzZ <138040888+iTzHeXzZ@users.noreply.github.com>
2023-12-08 01:24:42 +0100

    jungeeee

--- database/migrations/2023_12_06_173418_convert_sqlite_to_mysql.php
+++ database/migrations/2023_12_06_173418_convert_sqlite_to_mysql.php
@@ -28,9 +28,255 @@ return new class extends Migration
             echo "Datenbankverbindung fehlgeschlagen: " . $e->getMessage();
             die(); // Hier könntest du auch anders reagieren, je nach Anwendungsfall
         }
-        
-                $sqliteDataN2= DB::connection('sqlite')->table('project_user')->get();
+        // Beispiel: Erstelle eine neue MySQL-Tabelle
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('ort');
+            $table->string('postleitzahl');
+            $table->string('strasse');
+            $table->string('hausnummer');
+            $table->integer('wohneinheiten');
+            $table->integer('bestand');
+            $table->string('status');
+            $table->string('notiz', 255)->nullable();
+            $table->timestamps();
+        });
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+        Schema::create('failed_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->text('connection');
+            $table->text('queue');
+            $table->longText('payload');
+            $table->longText('exception');
+            $table->timestamp('failed_at')->useCurrent();
+        });
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('detail');
+            $table->timestamps();
+        });
+
+        $tableNames = config('permission.table_names');
+        $columnNames = config('permission.column_names');
+        $teams = config('permission.teams');
+
+        if (empty($tableNames)) {
+            throw new \Exception('Error: config/permission.php not loaded. Run [php artisan config:clear] and try again.');
+        }
+        if ($teams && empty($columnNames['team_foreign_key'] ?? null)) {
+            throw new \Exception('Error: team_foreign_key on config/permission.php not loaded. Run [php artisan config:clear] and try again.');
+        }
+
+        Schema::create($tableNames['permissions'], function (Blueprint $table) {
+            $table->bigIncrements('id'); // permission id
+            $table->string('name');       // For MySQL 8.0 use string('name', 125);
+            $table->string('guard_name'); // For MySQL 8.0 use string('guard_name', 125);
+            $table->timestamps();
+
+            $table->unique(['name', 'guard_name']);
+        });
+
+        Schema::create($tableNames['roles'], function (Blueprint $table) use ($teams, $columnNames) {
+            $table->bigIncrements('id'); // role id
+            if ($teams || config('permission.testing')) { // permission.testing is a fix for sqlite testing
+                $table->unsignedBigInteger($columnNames['team_foreign_key'])->nullable();
+                $table->index($columnNames['team_foreign_key'], 'roles_team_foreign_key_index');
+            }
+            $table->string('name');       // For MySQL 8.0 use string('name', 125);
+            $table->string('guard_name'); // For MySQL 8.0 use string('guard_name', 125);
+            $table->timestamps();
+            if ($teams || config('permission.testing')) {
+                $table->unique([$columnNames['team_foreign_key'], 'name', 'guard_name']);
+            } else {
+                $table->unique(['name', 'guard_name']);
+            }
+        });
+
+        Schema::create($tableNames['model_has_permissions'], function (Blueprint $table) use ($tableNames, $columnNames, $teams) {
+            $table->unsignedBigInteger(PermissionRegistrar::$pivotPermission);
+            $table->string('model_type');
+            $table->unsignedBigInteger($columnNames['model_morph_key']);
+            $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_permissions_model_id_model_type_index');
+
+            $table->foreign(PermissionRegistrar::$pivotPermission)
+                ->references('id') // permission id
+                ->on($tableNames['permissions'])
+                ->onDelete('cascade');
+            if ($teams) {
+                $table->unsignedBigInteger($columnNames['team_foreign_key']);
+                $table->index($columnNames['team_foreign_key'], 'model_has_permissions_team_foreign_key_index');
+
+                $table->primary([$columnNames['team_foreign_key'], PermissionRegistrar::$pivotPermission, $columnNames['model_morph_key'], 'model_type'],
+                    'model_has_permissions_permission_model_type_primary');
+            } else {
+                $table->primary([PermissionRegistrar::$pivotPermission, $columnNames['model_morph_key'], 'model_type'],
+                    'model_has_permissions_permission_model_type_primary');
+            }
+
+        });
+
+        Schema::create($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames, $columnNames, $teams) {
+            $table->unsignedBigInteger(PermissionRegistrar::$pivotRole);
+
+            $table->string('model_type');
+            $table->unsignedBigInteger($columnNames['model_morph_key']);
+            $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_roles_model_id_model_type_index');
+
+            $table->foreign(PermissionRegistrar::$pivotRole)
+                ->references('id') // role id
+                ->on($tableNames['roles'])
+                ->onDelete('cascade');
+            if ($teams) {
+                $table->unsignedBigInteger($columnNames['team_foreign_key']);
+                $table->index($columnNames['team_foreign_key'], 'model_has_roles_team_foreign_key_index');
+
+                $table->primary([$columnNames['team_foreign_key'], PermissionRegistrar::$pivotRole, $columnNames['model_morph_key'], 'model_type'],
+                    'model_has_roles_role_model_type_primary');
+            } else {
+                $table->primary([PermissionRegistrar::$pivotRole, $columnNames['model_morph_key'], 'model_type'],
+                    'model_has_roles_role_model_type_primary');
+            }
+        });
+
+        Schema::create($tableNames['role_has_permissions'], function (Blueprint $table) use ($tableNames) {
+            $table->unsignedBigInteger(PermissionRegistrar::$pivotPermission);
+            $table->unsignedBigInteger(PermissionRegistrar::$pivotRole);
+
+            $table->foreign(PermissionRegistrar::$pivotPermission)
+                ->references('id') // permission id
+                ->on($tableNames['permissions'])
+                ->onDelete('cascade');
+
+            $table->foreign(PermissionRegistrar::$pivotRole)
+                ->references('id') // role id
+                ->on($tableNames['roles'])
+                ->onDelete('cascade');
+
+            $table->primary([PermissionRegistrar::$pivotPermission, PermissionRegistrar::$pivotRole], 'role_has_permissions_permission_id_role_id_primary');
+        });
+
+        app('cache')
+            ->store(config('permission.cache.store') != 'default' ? config('permission.cache.store') : null)
+            ->forget(config('permission.cache.key'));
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('project_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->primary(['project_id', 'user_id']);
+        });
+
+        Schema::create('streets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+        });
+
+        Schema::table('project_user', function (Blueprint $table) {
+            $table->string('hausnummer')->nullable();
+        });
+
+                // Beispiel: Kopiere Daten von SQLite-Tabelle zu MySQL-Tabelle
+                $sqliteDataN = DB::connection('sqlite')->table('projects')->get();
+                foreach ($sqliteDataN as $record) {
+                    $record->created_at = now();
+                    if ($record->updated_at == '1.1.2000') {
+                        $record->updated_at = now();
+                    }
+                    if ($record->updated_at == '7.10.2023') {
+                        $record->updated_at = now();
+                    }
+                    if ($record->updated_at == '05.06.23') {
+                        $record->updated_at = now();
+                    }
+                    if ($record->updated_at == '17.10.2023') {
+                        $record->updated_at = now();
+                    }
+                    $record->notiz = substr($record->notiz, 0, 255);
+                    DB::connection('mysql')->table('projects')->insert((array) $record);
+                }
+                $sqliteDataN2= DB::connection('sqlite')->table('password_reset_tokens')->get();
                 foreach ($sqliteDataN2 as $record) {
+                    DB::connection('mysql')->table('password_reset_tokens')->insert((array) $record);
+                }
+                $sqliteDataN3= DB::connection('sqlite')->table('password_resets')->get();
+                foreach ($sqliteDataN3 as $record) {
+                    DB::connection('mysql')->table('password_resets')->insert((array) $record);
+                }
+                $sqliteDataN4= DB::connection('sqlite')->table('failed_jobs')->get();
+                foreach ($sqliteDataN4 as $record) {
+                    DB::connection('mysql')->table('failed_jobs')->insert((array) $record);
+                }
+                $sqliteDataN5= DB::connection('sqlite')->table('products')->get();
+                foreach ($sqliteDataN5 as $record) {
+                    DB::connection('mysql')->table('products')->insert((array) $record);
+                }
+                $sqliteDataN7= DB::connection('sqlite')->table('users')->get();
+                foreach ($sqliteDataN7 as $record) {
+                    DB::connection('mysql')->table('users')->insert((array) $record);
+                }
+                $sqliteDataN8= DB::connection('sqlite')->table('permissions')->get();
+                foreach ($sqliteDataN8 as $record) {
+                    DB::connection('mysql')->table('permissions')->insert((array) $record);
+                }
+                $sqliteDataN9= DB::connection('sqlite')->table('roles')->get();
+                foreach ($sqliteDataN9 as $record) {
+                    DB::connection('mysql')->table('roles')->insert((array) $record);
+                }
+                $sqliteDataN10= DB::connection('sqlite')->table('model_has_permissions')->get();
+                foreach ($sqliteDataN10 as $record) {
+                    DB::connection('mysql')->table('model_has_permissions')->insert((array) $record);
+                }
+                $sqliteDataN11= DB::connection('sqlite')->table('model_has_roles')->get();
+                foreach ($sqliteDataN11 as $record) {
+                    DB::connection('mysql')->table('model_has_roles')->insert((array) $record);
+                }
+                $sqliteDataN12= DB::connection('sqlite')->table('role_has_permissions')->get();
+                foreach ($sqliteDataN12 as $record) {
+                    DB::connection('mysql')->table('role_has_permissions')->insert((array) $record);
+                }
+                
+                $sqliteDataN14= DB::connection('sqlite')->table('personal_access_tokens')->get();
+                foreach ($sqliteDataN14 as $record) {
+                    DB::connection('mysql')->table('personal_access_tokens')->insert((array) $record);
+                }
+                
+                $sqliteDataN16= DB::connection('sqlite')->table('streets')->get();
+                foreach ($sqliteDataN16 as $record) {
+                    DB::connection('mysql')->table('streets')->insert((array) $record);
+                }
+
+                $sqliteDataN17= DB::connection('sqlite')->table('project_user')->get();
+                foreach ($sqliteDataN16 as $record) {
                     DB::connection('mysql')->table('project_user')->insert((array) $record);
                 }
 
