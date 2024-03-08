485e472e94becd46c7e1e7997989a40cf140be7f
iTzHeXzZ <138040888+iTzHeXzZ@users.noreply.github.com>
2024-03-08 12:32:29 +0100

    Create sendPdf.blade.php

---
+++ resources/views/emails/sendPdf.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Neuer Auftrag</title>
+</head>
+<body>
+    <h1>Hallo!</h1>
+    <p>Ein neuer Auftrag wurde erstellt. Sie finden die PDF-Dokument im Anhang.</p>
+    <p>Danke, <br>{{ $name }}</p>
+</body>
+</html>
