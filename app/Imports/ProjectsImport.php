fc372c67b972cb99f449ef0c7236a29b7dcad4bf
iTzHeXzZ <138040888+iTzHeXzZ@users.noreply.github.com>
2024-04-16 19:08:12 +0200

    better

--- app/Imports/ProjectsImport.php
+++ app/Imports/ProjectsImport.php
@@ -13,30 +13,40 @@ class ProjectsImport implements ToModel, WithHeadingRow, WithValidation
     public function model(array $row)
     {
         $existingProject = Project::where('ort', $row['ort'])
-                                  ->where('postleitzahl', $row['postleitzahl'])
-                                  ->where('strasse', $row['strasse'])
-                                  ->where('hausnummer', $row['hausnummer'])
-                                  ->first();
+            ->where('postleitzahl', $row['postleitzahl'])
+            ->first();
     
-        if (!$existingProject) {
-            $status = $row['bestand'] >= 1 ? 'Vertrag' : $row['status'];
+        
+        if ($existingProject) {
+            
+            $existingProject = Project::where('ort', $row['ort'])
+                ->where('postleitzahl', $row['postleitzahl'])
+                ->where('strasse', $row['strasse'])
+                ->where('hausnummer', $row['hausnummer'])
+                ->first();
     
-            return new Project([
-                'ort' => $row['ort'],
-                'postleitzahl' => $row['postleitzahl'],
-                'strasse' => $row['strasse'],
-                'hausnummer' => $row['hausnummer'],
-                'wohneinheiten' => $row['wohneinheiten'],
-                'bestand' => $row['bestand'],
-                'notiz' => $row['notiz'],
-                'status' => $status,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
+            
+            if ($existingProject) {
+                
+                return null;
+            }
         }
     
-        // Projekt überspringen, wenn bereits existiert
-        return null;
+        $status = !empty($row['status']) ? $row['status'] : 'Unbesucht';
+        $status = $row['bestand'] >= 1 ? 'Vertrag' : $row['status'];
+    
+        return new Project([
+            'ort' => $row['ort'],
+            'postleitzahl' => $row['postleitzahl'],
+            'strasse' => $row['strasse'],
+            'hausnummer' => $row['hausnummer'],
+            'wohneinheiten' => $row['wohneinheiten'],
+            'bestand' => $row['bestand'],
+            'notiz' => $row['notiz'],
+            'status' => $status,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
     public function rules(): array
